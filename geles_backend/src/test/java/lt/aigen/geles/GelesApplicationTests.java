0795e31ed08df12759cd808f0980db1cc9688a0d
Simonas Briedis <simas.briedis@gmail.com>
2021-04-07 19:02:25 +0300

    Initial project setup

---
+++ geles_backend/src/test/java/lt/aigen/geles/GelesApplicationTests.java
@@ -0,0 +1,13 @@
+package lt.aigen.geles;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GelesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
