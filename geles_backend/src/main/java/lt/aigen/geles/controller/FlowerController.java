7796516b312beefc4cc1a395b5c233e134260832
Emilis Kiškis <emilis.kiskis@mif.stud.vu.lt>
2021-04-18 19:44:28 +0300

    Created basic frontend for searching flowers

--- geles_backend/src/main/java/lt/aigen/geles/controller/FlowerController.java
+++ geles_backend/src/main/java/lt/aigen/geles/controller/FlowerController.java
@@ -5,13 +5,11 @@ import lt.aigen.geles.repositories.FlowerRepository;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-
-import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 import java.util.Optional;
 
-@RestController("/flowers")
+@RestController
+@RequestMapping("/flowers")
 public class FlowerController {
     FlowerRepository flowerRepository;
 
@@ -19,19 +17,12 @@ public class FlowerController {
         this.flowerRepository = flowerRepository;
     }
 
-    @GetMapping("/")
-    public String index() {
-        return "Greetings!";
-    }
-
-    @GetMapping("/search") // /flowers/?q=gele
-    @ResponseBody
-    public List<Flower> search(@RequestParam String q) {
-        return flowerRepository.findAllByName(q);
+    @GetMapping("/") // /flowers/?q=gele
+    public List<Flower> getFlowers(@RequestParam Optional<String> q) {
+        return flowerRepository.findAllByName(q.orElse(""));
     }
 
     @GetMapping("/{id}") // /flowers/10
-    @ResponseBody
     public ResponseEntity<Flower> getFlower(@PathVariable Long id) {
         return flowerRepository.findById(id).map(ResponseEntity::ok).orElse(new ResponseEntity<>(HttpStatus.NOT_FOUND));
     }
--- geles_backend/src/main/java/lt/aigen/geles/repositories/FlowerRepository.java
+++ geles_backend/src/main/java/lt/aigen/geles/repositories/FlowerRepository.java
@@ -3,13 +3,12 @@ package lt.aigen.geles.repositories;
 import lt.aigen.geles.models.Flower;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
-import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
 @Repository
 public interface FlowerRepository extends CrudRepository<Flower, Long> {
-    @Query("select fow from Flower fow where fow.name like %:name%")
-    List<Flower> findAllByName(@Param("name") String name);
+    @Query("from Flower f where f.name like %:name%")
+    List<Flower> findAllByName(String name);
 }
