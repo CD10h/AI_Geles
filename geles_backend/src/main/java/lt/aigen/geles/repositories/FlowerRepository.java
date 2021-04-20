8105eee0ca654aa3f32c83289203a8bf8e7e9c91
Om <simas.briedis@gmail.com>
2021-04-20 10:50:31 +0300

    Find expired flowers by date query

--- geles_backend/src/main/java/lt/aigen/geles/repositories/FlowerRepository.java
+++ geles_backend/src/main/java/lt/aigen/geles/repositories/FlowerRepository.java
@@ -3,12 +3,19 @@ package lt.aigen.geles.repositories;
 import lt.aigen.geles.models.Flower;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
+import java.util.Date;
 import java.util.List;
 
 @Repository
 public interface FlowerRepository extends CrudRepository<Flower, Long> {
     @Query("from Flower f where f.name like %:name%")
     List<Flower> findAllByName(String name);
+
+    @Query("from Flower f where f.expiryDate <= :searchDate")
+    List<Flower> findExpiredBeforeDate(@Param("searchDate") Date searchDate);
+
+
 }
