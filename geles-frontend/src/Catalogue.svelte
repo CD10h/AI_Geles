8357ddb7da9d8f8c4fd868bc3590a70ce3a636b2
Emilis Kiškis <kikis.emilis@gmail.com>
2021-04-23 20:22:51 +0300

    Moved flower list to separate file

---
+++ geles-frontend/src/Catalogue.svelte
@@ -0,0 +1,16 @@
+<script>
+  import type { Flower } from "./App.svelte";
+
+  // Variable to hold fetched list
+  export let flowers: Flower[];
+</script>
+
+<ul>
+  <!--
+    Map each `flowers` entry as an HTML element `li` 
+    When `flowers` updates, so does this block
+  -->
+  {#each flowers as flower}
+    <li>{flower.name}</li>
+  {/each}
+</ul>
