e1b1ab3c1a4f761291de0727fd22968538cd8209
Emilis Kiškis <emilis.kiskis@mif.stud.vu.lt>
2021-04-23 13:28:16 +0300

    Fixed import.sql formatting

--- geles_backend/src/main/resources/import.sql
+++ geles_backend/src/main/resources/import.sql
@@ -1,22 +1,11 @@
-INSERT INTO flower (id, name)
-VALUES (default, 'Frezija');
-INSERT INTO flower (id, name)
-VALUES (default, 'Gerbera');
-INSERT INTO flower (id, name)
-VALUES (default, 'Gvazdikas');
-INSERT INTO flower (id, name)
-VALUES (default, 'Hiacintas');
-INSERT INTO flower (id, name)
-VALUES (default, 'Kardelis');
-INSERT INTO flower (id, name)
-VALUES (default, 'Kalija');
-INSERT INTO flower (id, name)
-VALUES (default, 'Lelija');
-INSERT INTO flower (id, name)
-VALUES (default, 'Narcizas');
-INSERT INTO flower (id, name)
-VALUES (default, 'Orchidėja');
-INSERT INTO flower (id, name)
-VALUES (default, 'Rožė');
-INSERT INTO flower (id, name)
-VALUES (default, 'Tulpė');
+INSERT INTO flower (id, name) VALUES (default, 'Frezija');
+INSERT INTO flower (id, name) VALUES (default, 'Gerbera');
+INSERT INTO flower (id, name) VALUES (default, 'Gvazdikas');
+INSERT INTO flower (id, name) VALUES (default, 'Hiacintas');
+INSERT INTO flower (id, name) VALUES (default, 'Kardelis');
+INSERT INTO flower (id, name) VALUES (default, 'Kalija');
+INSERT INTO flower (id, name) VALUES (default, 'Lelija');
+INSERT INTO flower (id, name) VALUES (default, 'Narcizas');
+INSERT INTO flower (id, name) VALUES (default, 'Orchidėja');
+INSERT INTO flower (id, name) VALUES (default, 'Rožė');
+INSERT INTO flower (id, name) VALUES (default, 'Tulpė');
