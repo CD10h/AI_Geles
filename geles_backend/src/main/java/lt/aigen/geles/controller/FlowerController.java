1651dcf367105f3caf3a4f4611a3b1fdfdf955ae
Simonas Briedis <simas.briedis@gmail.com>
2021-04-27 10:09:05 +0300

    Added PUT mapping for flower

--- geles_backend/src/main/java/lt/aigen/geles/controller/FlowerController.java
+++ geles_backend/src/main/java/lt/aigen/geles/controller/FlowerController.java
@@ -34,4 +34,20 @@ public class FlowerController {
         var createdFlower = flowerRepository.save(newFlower);
         return new ResponseEntity<>(createdFlower, HttpStatus.CREATED);
     }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Flower> putFlower(@PathVariable Long id, @Valid @RequestBody Flower flowerData) {
+        var foundFlowerOpt = flowerRepository.findById(id);
+        if (foundFlowerOpt.isEmpty())
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+
+        var foundFlower = foundFlowerOpt.get();
+        foundFlower.setName(flowerData.getName());
+        foundFlower.setDescription(flowerData.getDescription());
+        foundFlower.setPrice(flowerData.getPrice());
+        foundFlower.setDaysToExpire(flowerData.getDaysToExpire());
+        foundFlower.setPhoto(flowerData.getPhoto());
+        flowerRepository.save(foundFlower);
+        return new ResponseEntity<>(foundFlower, HttpStatus.OK);
+    }
 }
