c56d93f6dbd5531a7d6e55846c55fcf97a24555c
titas251 <krtitas1@gmail.com>
2021-05-03 18:25:23 +0300

    Added svelte routing

--- geles-frontend/src/App.svelte
+++ geles-frontend/src/App.svelte
@@ -1,7 +1,19 @@
+<!-- App.svelte -->
 <script>
+  import { Router, Link, Route } from "svelte-routing";
   import SearchBar from "./SearchBar.svelte";
   import AddFlower from "./AddFlower.svelte";
+
+  export let url = "";
 </script>
 
-<SearchBar />
-<AddFlower />
+<Router {url}>
+  <nav>
+    <Link to="/add">Add Flower</Link>
+    <Link to="/search">Search</Link>
+  </nav>
+  <div>
+    <Route path="/add" component={AddFlower} />
+    <Route path="/search" component={SearchBar} />
+  </div>
+</Router>
