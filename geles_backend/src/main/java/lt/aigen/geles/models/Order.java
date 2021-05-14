3eb25d4e4be32750ada654b685e543582b981d99
Simonas Briedis <simas.briedis@gmail.com>
2021-05-14 18:56:22 +0300

    Made orderStatus user strings

--- geles_backend/src/main/java/lt/aigen/geles/models/Order.java
+++ geles_backend/src/main/java/lt/aigen/geles/models/Order.java
@@ -54,10 +54,6 @@ public class Order implements Serializable {
         PAID,
         CONFIRMED,
         DELIVERED,
-        CANCELED;
-        @JsonValue
-        public int toValue() {
-            return ordinal();
-        }
+        CANCELED
     }
 }
