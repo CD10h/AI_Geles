0a9f508784481d09ca6e6a616c34146b7ae2ef91
Emilis Kiškis <emilis.kiskis@mif.stud.vu.lt>
2021-05-15 19:14:10 +0300

    Added left margin to flower edit form

--- geles-frontend/src/UpdateFlower.svelte
+++ geles-frontend/src/UpdateFlower.svelte
@@ -59,48 +59,50 @@
   });
 </script>
 
-<h2>Redaguoti gėlę</h2>
-<form
-  on:submit={e => {
-    e.preventDefault();
-    handleSubmit();
-  }}
->
-  <Input
-    label="Pavadinimas"
-    bind:value={flower.name}
-    type="text"
-    name="name"
-  /><br /><br />
-  <Input
-    label="Kaina"
-    bind:value={flower.price}
-    type="number"
-    min={0}
-    step="0.01"
-    name="price"
-  /><br /><br />
-  <Input
-    label="Aprašymas"
-    bind:value={flower.description}
-    type="text"
-    name="description"
-  /><br /><br />
-  <Input
-    label="Galiojimo trukmė(dienomis)"
-    bind:value={flower.daysToExpire}
-    type="number"
-    min={1}
-    name="expirydate"
-  /><br /><br />
-  <button>Redaguoti</button>
-  {#each errors as error}
-    <p class="error">
-      <i class="mdi mdi-alert-circle" />
-      {error.slice(0, 1).toUpperCase()}{error.slice(1)}
-    </p>
-  {/each}
-</form>
+<div class="pagecontent">
+  <h2>Redaguoti gėlę</h2>
+  <form
+    on:submit={e => {
+      e.preventDefault();
+      handleSubmit();
+    }}
+  >
+    <Input
+      label="Pavadinimas"
+      bind:value={flower.name}
+      type="text"
+      name="name"
+    /><br /><br />
+    <Input
+      label="Kaina"
+      bind:value={flower.price}
+      type="number"
+      min={0}
+      step="0.01"
+      name="price"
+    /><br /><br />
+    <Input
+      label="Aprašymas"
+      bind:value={flower.description}
+      type="text"
+      name="description"
+    /><br /><br />
+    <Input
+      label="Galiojimo trukmė(dienomis)"
+      bind:value={flower.daysToExpire}
+      type="number"
+      min={1}
+      name="expirydate"
+    /><br /><br />
+    <button>Redaguoti</button>
+    {#each errors as error}
+      <p class="error">
+        <i class="mdi mdi-alert-circle" />
+        {error.slice(0, 1).toUpperCase()}{error.slice(1)}
+      </p>
+    {/each}
+  </form>
+</div>
 
 <style>
   .error {
